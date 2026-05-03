731bb00b1ded50f5d1cba537941e5da74da0056b
smanog <smanog@calpoly.edu>
2026-05-03 10:20:29 -0700

    class edits

--- proj2.py
+++ proj2.py
@@ -7,11 +7,19 @@ sys.setrecursionlimit(10_000)
 
 
 # Put your data definitions first!
-@dataclass
+@dataclass(frozen = True)
 class Row:
+    country: str|None
+    year: int|None
+    heat_emissions: float|None
+    heat_per_capita: float|None
+    energy_emissions: float|None
+    energy_per_capita: float|None
+    total_emissions: float|None
+    total_per_capita: float|None
     emissions: float
 
-@dataclass
+@dataclass(frozen = True)
 class Node:
     value: Row
     next: Node|None
@@ -19,7 +27,21 @@ class Node:
 
 # Then your functions.
 def read_csv_lines(filename: str) -> Optional[Node]:
-    pass
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)
+    return Node(read_csv_lines(filename))
 
 def listlen(data: Optional[Node]) -> int:
     pass
+#for reference for now
+def total_item_count(filename: str) -> int:
+    with open(filename, newline="") as csvfile:
+        iter = csv.reader(csvfile)
+        topline = next(iter)
+        if not (topline == expected_labels):
+            raise ValueError("unexpected first line: got: {}".format(topline))
+        item_count = 0
+        for line in iter:
+            item_count = item_count + float(line[2])
+        return item_count
